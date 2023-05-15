26a8ec8556a6dd76bfc19eab21af7cc76d519ade
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-16 02:54:28 +0800

    Mute audio player on mute btn click
    Change volume icon based on current volume levels. It could be high, low
    or muted. Make jazz genre the default selected audio source.

--- js/genre-choice.js
+++ js/genre-choice.js
@@ -11,7 +11,8 @@ const playBtn = document.querySelector(
 );
 
 
-let highlightedBtn;
+// console.log(genreBtns[0]);
+let highlightedBtn = genreBtns[0];
 
 // console.log(volumeSlider); 
 
@@ -55,22 +56,22 @@ playBtn.addEventListener("click", togglePlay);
 
 function playAudio() {
   audioPlayer.play();
-  changeIcon(playBtn.lastChild, "fa-play", "fa-pause");
+  changeIcon(playBtn.lastChild, "fa-pause");
   if (highlightedBtn) {
-    changeIcon(highlightedBtn.lastChild, "fa-circle-play", "fa-circle-pause");
+    changeIcon(highlightedBtn.lastChild, "fa-circle-pause");
   }
 }
 
 function pauseAudio() {
   audioPlayer.pause();
-  changeIcon(playBtn.lastChild, "fa-pause", "fa-play");
+  changeIcon(playBtn.lastChild, "fa-play");
   if (highlightedBtn) {
-    changeIcon(highlightedBtn.lastChild, "fa-circle-pause", "fa-circle-play");
+    changeIcon(highlightedBtn.lastChild, "fa-circle-play");
   }
 }
 
-function changeIcon(icon, from, to) {
-  icon.classList.remove(from);
+function changeIcon(icon, to) {
+  icon.classList.remove(icon.classList.item(1));
   icon.classList.add(to);
 }
 
@@ -82,10 +83,45 @@ const volumeSlider = document.querySelector(
   ".audio-control-container > input[type='range']"
 );
 
-updateVol();
-volumeSlider.addEventListener("input", updateVol);
+const muteBtn = document.querySelector(
+  ".audio-control-container > button:nth-child(2)"
+  );
+  
+  updateVol();
+  volumeSlider.addEventListener("input", updateVol);
+  
+  muteBtn.addEventListener("click", () => {
+    toggleVol();
+    
+  });
+  
+  
+  function updateVol() {
+    audioPlayer.volume = volumeSlider.value;
+    updateVolIcon();
+  }
+  
+function updateVolIcon() {
+  if (volumeSlider.value >= 0.25) {
+    changeIcon(muteBtn.lastChild, "fa-volume-high");
+  }
+  else if (volumeSlider.value == 0) {
+    changeIcon(muteBtn.lastChild, "fa-volume-xmark");
+  }
+  else {
+    changeIcon(muteBtn.lastChild, "fa-volume-low");
+  }
+}
 
 
-function updateVol() {
-  audioPlayer.volume = volumeSlider.value;
+let prevVol = 0.25;
+function toggleVol() {
+  if (volumeSlider.value != 0) {
+    prevVol = volumeSlider.value;
+    volumeSlider.value = 0;
+  }
+  else {
+    volumeSlider.value = prevVol;
+  }
+  updateVol();
 }
