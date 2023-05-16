9b8600f806d6c4cb3c246a6af720e923a0da86e7
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-05-17 01:57:55 +0800

    Make section fill up spaces in desktop view

--- js/responsiveness.js
+++ js/responsiveness.js
@@ -1,8 +1,8 @@
 const mainWrapper = document.querySelector(".main-wrapper");
-const sideNav = mainWrapper.querySelector(".side-nav");
-const sideNavContainer = sideNav.querySelector("ul");
+// const sideNav = mainWrapper.querySelector(".side-nav");
+// const sideNavContainer = sideNav.querySelector("ul");
 const footer = document.querySelector("footer");
-
+const main = document.querySelector("main")
 changeLayout();
 window.addEventListener("resize", changeLayout);
 
